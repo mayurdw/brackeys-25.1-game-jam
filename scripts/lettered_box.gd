219ff80836ab1dc3905c51358d10defe0677ce5c
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2025-02-17 21:19:08 +1300

    EOD 1: Box Sprite

--- scripts/lettered_box.gd
+++ scripts/lettered_box.gd
@@ -1,6 +1,10 @@
 class_name Box extends Node2D
 
 @export var box_word: String
+@onready var label: Label = $Sprite2D/Control/Label
+
+func _ready() -> void:
+	label.text = box_word
 
 func word_typed () -> void:
 	queue_free ()
