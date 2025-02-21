606fcfb128f3380df720cfedc560785a6a2fc7aa
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2025-02-21 19:10:12 +1300

    Level 1 Words ready

--- scripts/level_data.gd
+++ scripts/level_data.gd
@@ -2,26 +2,26 @@ class_name LevelData extends Node
 
 var tasks : Array [ Task ]
 @export var words : Array [ String ]
-@export var _inter_task_timer : int = 30 : get = _get_interval_time
-@export var _tasks_completed : int = 0
+@export var _inter_task_timer : int = 10 : get = get_interval_time
+@export var current_index : int = 0
 
-func _get_interval_time() -> int:
-	_inter_task_timer -= clamp( _tasks_completed * words.size() / 2, 1, _inter_task_timer - 1 )
+func get_interval_time() -> int:
+	_inter_task_timer = clamp( _inter_task_timer - current_index * 2, 2, 5 )
 	return _inter_task_timer
 	
-func task_completed() -> void:
-	_tasks_completed += 1
-	
 func are_there_more_tasks() -> bool:
-	return _tasks_completed < tasks.size()
+	return current_index < tasks.size()
 
 func next_task() -> Task:
-	return tasks[ _tasks_completed ]
+	var task := tasks [ current_index ]
+	current_index += 1
+	return task
 
 func _ready() -> void:
 	var index := 0
 	tasks.resize( words.size() )
-	tasks.fill ( Task.new() )
-	for task in tasks:
-		task.task_name = words[index]
+	for word in words:
+		var task = Task.new()
+		task.task_name = word
+		tasks[index] = task
 		index += 1
--- scripts/manager.gd
+++ scripts/manager.gd
@@ -4,6 +4,8 @@ var box_node := preload("res://scenes/lettered_box.tscn")
 
 @onready var display: Control = $HUD/TypedLetter
 @onready var task_ui_manager: Control = $"HUD/Task UI Manager"
+@onready var interval_timer: Timer = $"Interval Timer"
+
 @export var level_tasks : LevelData
 
 const _invalid_index := -1
@@ -12,11 +14,11 @@ const _invalid_selection := ""
 var word_typed_so_far := _invalid_selection
 var selected_index := _invalid_index
 var _list_of_words : Array [ Box ] = []
+var _caps_lock_enabled = false
 
 func _ready () -> void:
 	_list_of_words = task_ui_manager.get_list_of_boxes()
-	if level_tasks.are_there_more_tasks():
-		add_new_task( level_tasks.next_task() )
+	_on_timer_expiry()
 
 func _set_selection ( index: int = _invalid_index, character : String = _invalid_selection ) -> void:
 	selected_index = index
@@ -42,6 +44,7 @@ func _on_new_word ( character : String ) -> void:
 			_set_selection( index, character )
 			word.selected_count ( 1 )
 			return
+
 	print ( "Character Not Found in Words = " + character )
 
 func _on_back_space () -> void:
@@ -64,8 +67,12 @@ func _char_entered ( character : String ) -> void:
 
 func _input ( event: InputEvent ) -> void:
 	if event is InputEventKey and event.is_pressed():
-		if event.keycode >= KEY_A and event.keycode <= KEY_Z:
+		if KEY_CAPSLOCK == event.keycode:
+			_caps_lock_enabled = not _caps_lock_enabled
+		elif event.keycode >= KEY_A and event.keycode <= KEY_Z:
 			var character = String.chr ( event.keycode )
+			if not event.shift_pressed and not _caps_lock_enabled:
+				character = character.to_lower()
 			display.letter_typed ( character )
 			_char_entered( character )
 		elif KEY_ESCAPE == event.keycode:
@@ -74,21 +81,12 @@ func _input ( event: InputEvent ) -> void:
 			_set_selection()
 		elif KEY_BACKSPACE == event.keycode:
 			_on_back_space()
-		elif KEY_ENTER == event.keycode:
-			# TODO: This is test code
-			var task1 := Task.new()
-			task1.task_name = "Top Left"
-
-			var task2 := Task.new()
-			task2.task_name = "Top Right"
-			var task3 := Task.new()
-			task3.task_name = "Bottom Left"
-			add_tasks( [ task1, task2, task3 ] )
+		elif KEY_SHIFT == event.keycode:
+			pass
 		else:
 			print ( "Event Key Pressed = " + str ( event.keycode ) )
 
 func _on_task_completed ( box: Box ) -> void:
-	level_tasks.task_completed()
 	_list_of_words.erase( box )
 	box.word_typed()
 	_set_selection()
@@ -101,7 +99,17 @@ func add_new_task ( task: Task ) -> void:
 	box_instance.task = task
 	task_ui_manager.add_new_box ( box_instance )
 	_list_of_words.append( box_instance )
+	_start_interval_timer()
 
 func add_tasks ( tasks: Array [ Task ] ) -> void:
 	for task in tasks:
 		add_new_task ( task )
+
+func _start_interval_timer( ) -> void:
+	interval_timer.stop()
+	interval_timer.wait_time = level_tasks.get_interval_time()
+	interval_timer.start()
+
+func _on_timer_expiry() -> void:
+	if level_tasks.are_there_more_tasks():
+		add_new_task( level_tasks.next_task() )
