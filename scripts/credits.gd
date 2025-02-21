3e8eda7f73757064e553b1019d50f03b6cdf126c
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2025-02-22 12:17:17 +1300

    Art work for the level

---
+++ scripts/credits.gd
@@ -0,0 +1,5 @@
+extends Node
+
+var floor_art := "https://opengameart.org/content/lpc-floors"
+var furniture := "https://opengameart.org/content/indoor-office-appliances"
+var doors := "https://opengameart.org/content/doors"
