e13acd09913c7fa473bf5ca085e711deab103e79
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2025-02-23 11:44:24 +1300

    Credits Menu Navigation

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -5,17 +5,22 @@ extends Control
 @onready var credits: TypingBox = $CenterContainer/VBoxContainer/Menu/Credits
 @onready var quit: TypingBox = $CenterContainer/VBoxContainer/Menu/Quit
 
+@export var startText = "Start"
+@export var creditsText = "Credits"
+@export var quitText = "Quit"
+
 func _ready() -> void:
-	start.set_box_text( "Start" )
-	credits.set_box_text( "Credits" )
-	quit.set_box_text( "Quit" )
+	start.set_box_text( startText )
+	credits.set_box_text( creditsText )
+	quit.set_box_text( quitText )
 	var words: Array [ TypingBox ] = [ start, credits, quit ]
 	manager.set_initial_list_of_words( words )
 
 
 func on_word_selected( word: TypingBox ) -> void:
-	print ( "Word selected = " + str ( word.box_text ) )
-
+	pass
 
 func on_word_typed(word: TypingBox) -> void:
 	print ( "Word typed = " + str ( word.box_text ) )
+	if creditsText == word.box_text:
+		get_tree().change_scene_to_file("res://scenes/credits.tscn")
